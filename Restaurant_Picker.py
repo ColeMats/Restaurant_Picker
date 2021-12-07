28bdacc6e582b933dd91f6640d254de9251f3707
Cole Matsueda <cmatsueda@chapman.edu>
2021-12-07 15:37:51 -0800

    Add function works and started working on home function

--- Restaurant_Picker.py
+++ Restaurant_Picker.py
@@ -8,10 +8,16 @@ def add_restaurant(restaurant, file_name):
         with open(file_name, 'r') as file:
             lines = file.readlines()
             for i in lines:
-                if (i.strip('\n') != restaurant):
-                    f = open(file_name, 'a')
-                    f.write(restaurant + '\n')
-                    f.close()
+                if (i.strip('\n') == restaurant):
+                    print('Restaurant is already stored')
+                    t = True
+                    break
+                else:
+                    t = False
+            if (t == False):
+                f = open(file_name, 'a')
+                f.write(restaurant + '\n')
+                f.close()
     except:
         f = open(file_name, 'a')
         f.write(restaurant + '\n')
@@ -39,12 +45,16 @@ def Pick_Rand_Rest(file_name):
 
 def home():
     #user can choose to generate a random restaurant from the stored restaurants
-    #Pick_Rand_Rest('restaurant.txt')
 
+    user_input = input('Welcome to Restaurant Picker! \n Please type the number for the following: \n 1 - Generate a random restaurant \n 2 - Add a restaurant \n 3 - Delete a restaurant \n 4 - View all Restaurants \n')
+    if (user_input == '1'):
+        Pick_Rand_Rest('restaurant.txt')
     #user can add a restaurant
-    add_restaurant('Subway', 'restaurant.txt')
-    #add_restaurant('Panda Express', 'restaurant.txt')
-    #add_restaurant('Zippys', 'restaurant.txt')
+    if (user_input == '2'):
+        user_rest = input("Which restaurant(s) would you like to add (seperated by commas)? \n")
+        rests = user_rest.split(', ')
+        for i in rests:
+            add_restaurant(i, 'restaurant.txt')
 
     #user can delete a restaurant
     #delete_restaurant('Burger King', 'restaurant.txt')
